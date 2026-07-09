a1d91437619589f0c970327ad711eb11dad299c6
Lorena Hendrix <lorenahendrix163@gmail.com>
2026-07-09 13:34:40 +0200

    corrected battmo dependensie version

--- dependencies.jl
+++ dependencies.jl
@@ -2,11 +2,11 @@ using Pkg
 Pkg.update()
 
 Pkg.add(PackageSpec(name="HTTP", version="1.10.19"))
-Pkg.add(PackageSpec(name="BattMo", version="0.1.0"))
+Pkg.add(PackageSpec(name="BattMo", version="0.1.9"))
 Pkg.add(PackageSpec(name="Jutul", version="0.2.42"))
 Pkg.add(PackageSpec(name="JSON", version="1.2.0"))
 Pkg.add(PackageSpec(name="HDF5", version="0.17.2"))
-Pkg.add(PackageSpec(name="PackageCompiler", version="2.1.22"))
+Pkg.add(PackageSpec(name="PackageCompiler"))
 Pkg.add(PackageSpec(name="UUIDs", version = "1.11.0"))
 
 

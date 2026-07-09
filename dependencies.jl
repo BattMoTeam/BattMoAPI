ed97cd4c2e8453010fe2869428c75571233b2ab6
Lorena Hendrix <lorenahendrix163@gmail.com>
2026-07-09 12:09:55 +0200

    added dependency package versions

--- dependencies.jl
+++ dependencies.jl
@@ -1,12 +1,14 @@
 using Pkg
 Pkg.update()
-Pkg.add("HTTP")
-Pkg.add("BattMo")
-Pkg.add("Jutul")
-Pkg.add("JSON")
-Pkg.add("HDF5")
-Pkg.add("PackageCompiler")
-Pkg.add("UUIDs")
+
+Pkg.add(PackageSpec(name="HTTP", version="1.10.19"))
+Pkg.add(PackageSpec(name="BattMo", version="0.1.0"))
+Pkg.add(PackageSpec(name="Jutul", version="0.2.42"))
+Pkg.add(PackageSpec(name="JSON", version="1.2.0"))
+Pkg.add(PackageSpec(name="HDF5", version="0.17.2"))
+Pkg.add(PackageSpec(name="PackageCompiler", version="2.1.22"))
+Pkg.add(PackageSpec(name="UUIDs", version = "1.11.0"))
+
 
 
 using HTTP, BattMo, JSON, Jutul, UUIDs, HDF5, PackageCompiler

c90e1475af64a51836e5a17d209859dcedd1ea90
Lorena Hendrix <lorenahendrix163@gmail.com>
2026-07-09 13:55:22 +0200

    removed jutul version

--- dependencies.jl
+++ dependencies.jl
@@ -3,7 +3,7 @@ Pkg.update()
 
 Pkg.add(PackageSpec(name="HTTP", version="1.10.19"))
 Pkg.add(PackageSpec(name="BattMo", version="0.1.9"))
-Pkg.add(PackageSpec(name="Jutul", version="0.2.42"))
+Pkg.add(PackageSpec(name="Jutul"))
 Pkg.add(PackageSpec(name="JSON", version="1.2.0"))
 Pkg.add(PackageSpec(name="HDF5", version="0.17.2"))
 Pkg.add(PackageSpec(name="PackageCompiler"))

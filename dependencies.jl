b2ca0844710ca8077c8db534962b44071bd73c17
Lorena Hendrix <lorenahendrix163@gmail.com>
2026-07-10 08:28:09 +0200

    correct dependencies

--- dependencies.jl
+++ dependencies.jl
@@ -3,9 +3,9 @@ Pkg.update()
 
 Pkg.add(PackageSpec(name="HTTP", version="1.10.19"))
 Pkg.add(PackageSpec(name="BattMo", version="0.1.9"))
-Pkg.add(PackageSpec(name="Jutul"))
-Pkg.add(PackageSpec(name="JSON", version="1.2.0"))
-Pkg.add(PackageSpec(name="HDF5", version="0.17.2"))
+Pkg.add(PackageSpec(name="Jutul", version="0.3.11"))
+Pkg.add(PackageSpec(name="JSON", version="1.6.1"))
+Pkg.add(PackageSpec(name="HDF5", version="0.17.3"))
 Pkg.add(PackageSpec(name="PackageCompiler"))
 Pkg.add(PackageSpec(name="UUIDs", version = "1.11.0"))
 
